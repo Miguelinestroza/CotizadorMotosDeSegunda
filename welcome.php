562722054c11088ea73c4d329f298b27bde6c1c5
Miguelinestroza <135190305+Miguelinestroza@users.noreply.github.com>
2024-06-27 17:26:26 -0600

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -41,12 +41,12 @@ echo "Bienvenido " . $_SESSION['email'];
             <tr>
                 <td colspan="3"></td>
                 <td>
-                <font size="1"><center><label for="fecha">fecha</label></center></font>
+                <font size="1"><center><label id="fechaActual">fecha</label></center></font>
                 </td>
             </tr>
         
             <tr>
-                <td colspan="4" style="background-color: rgb(36, 64, 98);">
+                <td colspan="4" style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>DATOS DEL CLIENTE</b></font></center>
                 </td>
             </tr>
@@ -54,8 +54,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Nombre cliente:</b></center>
                 </td>
-                <td colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text" name="Nombre" required/>
+                <td colspan="2">
+                    <input type="text" name="Nombre" required style="width: 100%; text-align: center; background-color: rgb(235, 241, 222)" />
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -65,8 +65,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Identidad o RTN:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;" />
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -76,8 +76,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Lugar:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;" />
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -87,8 +87,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Municipio:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -98,8 +98,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Departamento:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -109,8 +109,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Teléfono:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -120,8 +120,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Oficio:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -131,15 +131,15 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Estado civil:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
                 </td>
             </tr>
             <tr>
-                <td colspan="4" style="background-color: rgb(36, 64, 98);">
+                <td colspan="4" style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>DATOS DEL AVAL</b></font></center>
                 </td>
             </tr>
@@ -147,8 +147,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Nombre de aval:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -158,15 +158,15 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Ident. De aval:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
                 </td>
             </tr>
             <tr>
-                <td colspan="4" style="background-color: rgb(36, 64, 98);">
+                <td colspan="4" style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>DATOS DEL VEHICULO</b></font></center>
                 </td>
             </tr>
@@ -207,7 +207,7 @@ echo "Bienvenido " . $_SESSION['email'];
                     <center><b>Motor:</b></center>
                 </td>
                 <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <select name="Motos" id="">
+                    <select name="Motos" id="" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;">
                         <option value=""></option>
                         <option value="BK 150">BK 150</option>
                         <option value="BK 150 2">BK 150 2</option>
@@ -249,7 +249,7 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td  colspan="4"></td>
             </tr>  
             <tr>
-                <td colspan="4" style="background-color: rgb(36, 64, 98);">
+                <td colspan="4" style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>COSTO Y FINANCIAMIENTO</b></font></center>
                 </td>
             </tr>
@@ -265,8 +265,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Prima:</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -316,8 +316,8 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td colspan="1" style="background-color: rgb(196, 215, 155);">
                     <center><b>Cant. Meses</b></center>
                 </td>
-                <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <input type="text">
+                <td  colspan="2">
+                    <input type="text" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;"/>
                 </td>
                 <td colspan="1">
                     <font color="red"><b>Digitar</b></font>
@@ -328,7 +328,7 @@ echo "Bienvenido " . $_SESSION['email'];
                     <center><b>Forma de Pago</b></center>
                 </td>
                 <td  colspan="2" style="background-color: rgb(235, 241, 222);">
-                    <select name="Motos" id="">
+                    <select name="Motos" id="" required style="width: 100%; background-color: rgb(235, 241, 222);text-align: center;">
                         <option value=""></option>
                         <option value="PAGOS MENSUALES">PAGOS MENSUALES</option>
                         <option value="PAGOS QUINCENALES">PAGOS QUINCENALES</option>
@@ -343,21 +343,21 @@ echo "Bienvenido " . $_SESSION['email'];
                 <td  colspan="4"></td>
             </tr>
             <tr>
-                <td colspan="4" style="background-color: rgb(36, 64, 98);">
+                <td colspan="4" style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>PLAN DE PAGO ACEPTADO POR EL CLIENTE</b></font></center>
                 </td>
             </tr>
             <tr>
-                <td style="background-color: rgb(36, 64, 98);">
+                <td style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>Cuotas.</b></font></center>
                 </td>
-                <td style="background-color: rgb(36, 64, 98);">
+                <td style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>Formas de Pagos.</b></font></center>
                 </td>
-                <td style="background-color: rgb(36, 64, 98);">
+                <td style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>Valor De Cuota.</b></font></center>
                 </td>
-                <td style="background-color: rgb(36, 64, 98);">
+                <td style="background-color: rgb(36, 64, 100);">
                     <center><font color="white"><b>Saldo Final.</b></font></center>
                 </td>
             </tr>
@@ -375,8 +375,101 @@ echo "Bienvenido " . $_SESSION['email'];
                     <center><label for="saldoFinal">0</label></center>
                 </td>
             </tr>
+            <tr>
+                <td colspan="4">
+                    
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="background-color: rgb(149, 179, 215);">
+                    <center><b>Nota: Cotizacion valida por 8 dias.</b></center>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <center>________________________________</center>
+                </td>
+                <td colspan="2">
+                    <center>________________________________</center>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <center>Firma Del Cliente</center>
+                </td>
+                <td colspan="2">
+                    <center>(f) Asesor De Ventas</center>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    
+                </td>
+                <td colspan="1">
+                    <center>Nombre:</center>
+                </td>
+                <td colspan="1">
+                    ________________
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    
+                </td>
+                <td colspan="1">
+                    <center>Tel:</center>
+                </td>
+                <td colspan="1">
+                    ________________
+                </td>
+            </tr>
         </table>
     </form>
     </div>
+    <script>
+        // Obtener la fecha actual
+        let today = new Date();
+
+        // Días de la semana y meses en español
+        const weekdays = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        const months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+        // Obtener cada parte de la fecha
+        let weekday = weekdays[today.getDay()];
+        let day = today.getDate();
+        let month = months[today.getMonth()];
+        let year = today.getFullYear();
+
+        // Construir la fecha formateada
+        let formattedDate = `${weekday} ${day} de ${month} del ${year}`;
+
+        // Mostrar la fecha formateada en el elemento div con id "fechaActual"
+        document.getElementById('fechaActual').textContent = formattedDate;
+    </script>
 </body>
 </html>
